16e5d44c9a743103d147a86f38636db09d110caf
Sebastian Gradinaru <gradinaru.sebastian1@gmail.com>
2023-03-10 20:48:10 +0200

    added fontawesome

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home-page',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+
+  faCoffee = faCoffee;
 
   constructor() { }
 
