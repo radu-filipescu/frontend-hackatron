594e9ffae23270953955a82e7624adb515aad987
Sebastian Gradinaru <gradinaru.sebastian1@gmail.com>
2023-03-11 22:48:01 +0200

    draw nodes + edges

--- src/app/pages/simulation-page/simulation-page.component.ts
+++ src/app/pages/simulation-page/simulation-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { faUserPlus, faPlus, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { nodeDTO } from 'src/app/shared/DTOs/NodeDTO';
 import { NodesService } from 'src/app/shared/services/nodes.service';
@@ -14,6 +14,9 @@ export class SimulationPageComponent implements OnInit {
   faUserPlus = faUserPlus;
   faPlus = faPlus;
   faXmark = faXmark;
+
+  @ViewChild('canvas')
+  canvas!: ElementRef<HTMLCanvasElement>;
 
   networkInitialized: boolean = false;
   addingNodeModalVisible: boolean = false;
@@ -46,6 +49,15 @@ export class SimulationPageComponent implements OnInit {
   convertDTOtoNodesInternal(nodes: nodeDTO[]) {
     let result: nodeInternal[] = [];
 
+    var alpha = 0;
+    let originX = 770; //origin x and y
+    let originY = 400;
+    let r = 350;
+
+    nodes.push(nodes[0])
+    nodes.push(nodes[0])
+    nodes.push(nodes[0])
+
     for(let i = 0; i < nodes.length; i++) {
       let newNodeInternal = new nodeInternal();
 
@@ -54,24 +66,56 @@ export class SimulationPageComponent implements OnInit {
       newNodeInternal.balance = nodes[i].balance;
       newNodeInternal.password = nodes[i].password;
       newNodeInternal.isMining = nodes[i].isMining = (i == 0) ? true : false;
-      newNodeInternal.displayX = (i * 3 + 2) * 100;
-      newNodeInternal.displayY = 100;
 
+      newNodeInternal.displayX = originX + Math.cos(alpha) * r;
+      newNodeInternal.displayY = originY + Math.sin(alpha) * r;
+      
       result.push(newNodeInternal);
+
+      alpha += 2 * Math.PI / nodes.length;
     }
 
     return result;
+  }
+
+  onRightClick(event: any) {
+    event.preventDefault();
   }
 
   refreshNodes() {
     this.nodeService.getAllNodes()
       .subscribe(nodes => {
         this.networkNodes = this.convertDTOtoNodesInternal(nodes);
+        this.drawConnections();
       });
   }
 
-  onRightClick(event: any) {
-    event.preventDefault();
+  getXDraw(actualX: number) {
+    return Math.ceil(actualX + 20)
+  }
+
+  getYDraw(actualY: number) {
+    return Math.ceil(actualY + 24)
+  }
+
+  drawConnections(){
+    var ctx = this.canvas.nativeElement.getContext('2d');
+    if(!ctx) {
+      return;
+    }
+    ctx.beginPath();
+
+    for(var i = 0; i < this.networkNodes.length; i++) {
+      for(var j = 0; j < this.networkNodes.length; j++){
+        if(i != j){
+          console.log(this.networkNodes[i], this.networkNodes[j])
+          ctx.moveTo(this.getXDraw(this.networkNodes[i].displayX), this.getYDraw(this.networkNodes[i].displayY));
+          ctx.lineTo(this.getXDraw(this.networkNodes[j].displayX), this.getYDraw(this.networkNodes[j].displayY));
+        }
+      }
+    }
+    
+    ctx.stroke();
   }
 
 }
